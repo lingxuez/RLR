5853334a548dace530099b995334b2e955a92e8a
Lingxue Zhu <lingxue.zhu@gmail.com>
2016-10-09 13:13:40 -0400

    clean up run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/user/bin/bash
 
-#######################
+##############################
+## Lingxue Zhu
+## 2016/10/09
+##############################
 ##
 ## On-line stochastic gradient descent
 ## for L2-regularized logistic regression
@@ -15,24 +18,25 @@
 ##
 ## Names for the labels are hard-coded in LR.java
 ## Multiple binary classifiers are trained, one for each label
+##
 ##############################
 
 ## path to training and testing data
 TRAIN=../data/abstract.small.train
 TEST=../data/abstract.small.test
-## number of training samples
+## number of training samples, i.e., wc -l $TRAIN
 SIZE=44925 
-## iteration for SGD
+## iteration to perform for SGD
 ITER=20 
-## coeff for L2 regularization
+## coefficient for L2 regularization
 REGCOEFF=0.1
-## vocabulary size for hash trick
+## vocabulary size for hashing trick
 DICSIZE=10000
 
 ## run
 javac LR.java
 
-for((i=1;i<=40;i++));
-do /usr/local/bin/gshuf $TRAIN;
+for((i = 1; i <= $ITER; i++));
+do /usr/local/bin/gshuf $TRAIN; ## randomly shuffle data
 done | java -Xmx128m LR $DICSIZE 0.5 $REGCOEFF $ITER $SIZE $TEST > prediction.txt
 
